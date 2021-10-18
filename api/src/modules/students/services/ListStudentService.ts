a40927427d5eec57eba63e096478569155572981
Bruno Mesquita <brunocmesquita@gmail.com>
2021-10-18 17:59:35 -0300

    Inserido lib para paginação

--- api/src/modules/students/services/ListStudentService.ts
+++ api/src/modules/students/services/ListStudentService.ts
@@ -2,13 +2,23 @@ import { getCustomRepository } from 'typeorm';
 import Student from '../typeorm/entities/Student';
 import { StudentRepository } from '../typeorm/repositories/StudentsRepository';
 
+interface IPaginateStudent {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
 class ListStudentService {
-  public async execute(): Promise<Student[]> {
+  public async execute(): Promise<IPaginateStudent> {
     const studentsRepository = getCustomRepository(StudentRepository);
 
-    const users = await studentsRepository.find();
+    const users = await studentsRepository.createQueryBuilder().paginate();
 
-    return users;
+    return users as IPaginateStudent;
   }
 }
 
